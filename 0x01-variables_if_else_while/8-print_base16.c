f2e06ce7d6de001ecfb16424055b6fc17c355835
Bukunmi Ojo-David <bukunmidavies@yahoo.com>
2022-07-04 18:51:19 -0700

    Second project progress

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - numbers program
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+        int x;
+
+        for (x = '0'; x <= '9'; x++)
+        {
+                putchar(x);
+        }
+        for (x = 0; x < 6; x++)
+        {
+                putchar(x + 'a');
+        }
+        putchar('\n');
+        return (0);
+}
+
