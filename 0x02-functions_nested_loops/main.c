b304c8824141c683757133e63cb3c0953aff42db
Bukunmi Ojo-David <bukunmidavies@yahoo.com>
2022-07-06 17:09:30 -0700

    Progressive

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,12 +1,23 @@
 #include "main.h"
 
+
+
 /**
- * main-calls other functions
  *
- * Return: 0
+ *  * main - check the code
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
  */
+
 int main(void)
+
 {
-print something(void);
-return (0);
+
+	    print_alphabet();
+
+	    return (0);
+
 }
