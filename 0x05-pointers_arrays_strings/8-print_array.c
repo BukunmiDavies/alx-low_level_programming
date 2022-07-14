e0db0d6627ea1e906d48e4af4ec9a908a1086846
Bukunmi Ojo-David <bukunmidavies@yahoo.com>
2022-07-13 19:13:24 -0700

    Progress

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * puts_half - half of a string.
  * @str: string to be printed.
@@ -9,13 +8,14 @@ void puts_half(char *str)
 	int i = 0, len = 0, j;
 
 	while (str[i++])
+
 		len++;
 
 	if ((len % 2) == 0)
 		j = len / 2;
 	
 	else
-		j = (len + 1) /2;
+		j = (len + 1) / 2;
 
 	for (i = j; i < len; i++)
 		_putchar(str[i]);
